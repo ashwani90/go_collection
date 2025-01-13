a9ca3cb882c8546dd14a44fd829cad42091ea7f9
Ashwani <ashwani90125@gmail.com>
2025-01-13 22:28:53 +0530

    Work on add interfce implementation

--- cha_11/interface.go
+++ cha_11/interface.go
@@ -15,6 +15,25 @@ type Product struct {
 	price float64
 }
 
+func (p Product) getName() string {
+	return p.name
+}
+
+func (p Product) getCost(_ bool) float64 {
+	return p.price
+}
+
+func (s Service) getName() string {
+	return s.description
+}
+
+func (s Service) getCost(recur bool) float64 {
+	if (recur) {
+		return s.monthlyFee * float64(s.durationMonths)
+	}
+	return s.monthlyFee
+}
+
 type Expense interface {
 	getName() string
 	getCost(annual bool) float64
@@ -26,4 +45,15 @@ func main() {
 
 	fmt.Println("Product: ", kayak.name, "Price: ", kayak.price)
 	fmt.Println("Service: ", insurance.description, "Price: ", insurance.monthlyFee*float64(insurance.durationMonths))
+
+
+	// using the interface
+	expenses := []Expense {
+		Product {"Kayak", "Watersports", 275},
+		Service {"Boat Cover", 12, 89.50},
+	}
+
+	for _, expense := range expenses {
+		fmt.Println("Expense: ", expense.getName(), " Cost: ", expense.getCost(true))
+	}
 }
