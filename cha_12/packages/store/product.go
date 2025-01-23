2651d34bad1990881032d47034b6ca63dce69c65
Ashwani <ashwani90125@gmail.com>
2025-01-23 13:08:38 +0530

    Work on package handlign

--- cha_12/packages/store/product.go
+++ cha_12/packages/store/product.go
@@ -1,21 +1,20 @@
 package store
 
+var standardTax = newTaxRate(0.25, 20)
+
 type Product struct {
-	// Exported fields
-	Name, Category string
-	// Unexported Fields
-	price float64
+    Name, Category string
+    price float64 
 }
 
 func NewProduct(name, category string, price float64) *Product {
-	return &Product{ name, category, price }
+    return &Product{ name, category, price }
 }
 
 func (p *Product) Price() float64 {
-	return p.price
+    return standardTax.calcTax(p)
 }
 
-func (p *Product) SetPrice(newPrice float64) {
-	p.price = newPrice
+func (p *Product) SetPrice(newPrice float64)  {
+    p.price = newPrice
 }
-
