bdf2c9a124d7e7a21e2e0b0915b8f1e257a6b21f
Ashwani <ashwani90125@gmail.com>
2025-01-27 00:25:38 +0530

    Work on add product type

--- cha_13/composition/store/product.go
+++ cha_13/composition/store/product.go
@@ -9,3 +9,7 @@ func (p *Product) Price(taxRate float64) float64 {
 	return p.price + (p.price*taxRate)
 }
 
+func NewProduct(name, category string, price float64) *Product {
+	return &Product { name, category, price}
+}
+
