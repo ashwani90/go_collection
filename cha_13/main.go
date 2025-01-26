3709c5cccf768a7dcba0369b098e6d4b5c003ee4
Ashwani <ashwani90125@gmail.com>
2025-01-27 00:28:24 +0530

    Work on add product type

--- cha_13/main.go
+++ cha_13/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"composition/store"
+)
 
 func main() {
 	fmt.Println("Hello type and interface composition")
+	kayak := store.NewProduct("Kayak", "Watersports", 275)
+	lifejacket := &store.Product { Name: "Lifejacket", Category: "Watersports"}
+
+	for _, p := range []*store.Product { kayak, lifejacket} {
+		fmt.Println("Name: ", p.Name, "Category: ", p.Category, "Price: ", p.Price(0.2))
+	}
 }
