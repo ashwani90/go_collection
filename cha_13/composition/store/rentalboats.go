c0b679a5147c7f09c4b8a46986eced5ac8dcae6e
Ashwani <ashwani90125@gmail.com>
2025-01-28 11:49:19 +0530

    Work on add boat compositie type

--- cha_13/composition/store/rentalboats.go
+++ cha_13/composition/store/rentalboats.go
@@ -1,10 +1,15 @@
 package store
 
+type Crew struct {
+	Captain, FirstOfficer string
+}
+
 type RentalBoat struct {
 	*Boat
 	IncludeCrew bool
+	*Crew
 }
 
-func NewRentalBoat(name string, price float64, capacity int, motorized, crewed bool) *RentalBoat {
-	return &RentalBoat { NewBoat(name, price, capacity, motorized), crewed}
+func NewRentalBoat(name string, price float64, capacity int, motorized, crewed bool, captain, firstOfficer string) *RentalBoat {
+	return &RentalBoat { NewBoat(name, price, capacity, motorized), crewed, &Crew{captain, firstOfficer}}
 }
