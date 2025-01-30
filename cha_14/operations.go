c0bae18f0871ffd1824ed46442b8b534b75e94fb
Ashwani <ashwani90125@gmail.com>
2025-01-30 14:56:37 +0530

    Work on product groups by categoru

--- cha_14/operations.go
+++ cha_14/operations.go
@@ -13,6 +13,7 @@ func CalcStoreTotal(data ProductData) {
 
 func (group ProductGroup) TotalPrice(category string,) (total float64) {
 	for _,p := range group {
+		fmt.Println(category, "product: ", p.Name)
 		total += p.Price
 	}
 	fmt.Println(category, "Subtotal:", ToCurrency(total))
