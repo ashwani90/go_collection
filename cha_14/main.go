f898d7e931a1cf1fbb1ca1387adc692d11a1fe11
Ashwani <ashwani90125@gmail.com>
2025-01-30 14:59:29 +0530

    Work on product groups by categoru

--- cha_14/main.go
+++ cha_14/main.go
@@ -1,11 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 // use go run . to run the project
 
 func main() {
 	fmt.Println("main function started")
 	CalcStoreTotal(Products)
+	time.Sleep(time.Second*5)
 	fmt.Println("main function ended")
 }
