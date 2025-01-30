16bf4c2d897d79e037d0a52f7f641dd5cfe41927
Ashwani <ashwani90125@gmail.com>
2025-01-30 15:02:37 +0530

    Work on product groups by categoru

--- cha_14/operations.go
+++ cha_14/operations.go
@@ -18,19 +18,21 @@ import (
 
 func CalcStoreTotal(data ProductData) {
 	var storeTotal float64
-
+	var channel chan float64 = make(chan float64)
 	for category, group := range data {
 		go group.TotalPrice(category)
 	}
 	fmt.Println("Total: ", ToCurrency(storeTotal))
 }
 
-func (group ProductGroup) TotalPrice(category string,) (total float64) {
+func (group ProductGroup) TotalPrice(category string, resultChannel chan float64) {
+	var total float64
 	for _,p := range group {
 		fmt.Println(category, "product: ", p.Name)
 		total += p.Price
 		time.Sleep(time.Millisecond*1000)
 	}
 	fmt.Println(category, "Subtotal:", ToCurrency(total))
+	resultChannel<-total
 	return
 }
