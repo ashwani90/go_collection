d178fc930f4c40b6f5b22633b38b7aadd4d39ecb
Ashwani <ashwani90125@gmail.com>
2025-02-01 12:43:09 +0530

    Work on fix data issue

--- cha_14/main.go
+++ cha_14/main.go
@@ -26,6 +26,22 @@ func main() {
 
 	go DispatchOrders(chan<- DispatchNotification(dispatchChannel))
 	receiveDispatches((<-chan DispatchNotification)(dispatchChannel))
+
+	for {
+		select {
+		case details, ok:= <- dispatchChannel:
+			if ok {
+				fmt.Println("Dispatch to", details.Customer, ":", details.Quantity, 'x', details.Product.Name)
+			} else {
+				fmt.Println("Channel has been closed")
+				goto alldone
+			}
+		default:
+			fmt.Println("-- No message ready to be received")
+			time.Sleep(time.Millisecond*500)
+		}
+	}
+	alldone: fmt.Println("All values received")
 }
 
 func main2() {
