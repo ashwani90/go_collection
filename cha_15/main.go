58ec4d98c82e1897c48fdbcf805836e582023e94
Ashwani <ashwani90125@gmail.com>
2025-02-05 18:14:03 +0530

    Work on product slice setup

--- cha_15/main.go
+++ cha_15/main.go
@@ -5,9 +5,14 @@ import "fmt"
 func main() {
 	// fmt.Println("Hello error handling")
 
-	categories := []string {"Watersports", "Chess"}
+	categories := []string {"Watersports", "Chess", "Running" }
 	for _, cat := range categories {
-		total := Products.TotalPrice(cat)
-		fmt.Println(cat, "Total:", ToCurrency(total))
+		total, err := Products.TotalPrice(cat, )
+		if (err == nil) {
+			fmt.Println(cat, "Total:", ToCurrency(total))
+		} else {
+			fmt.Println(cat, "(no such category)")
+		}
+		
 	}
 }
