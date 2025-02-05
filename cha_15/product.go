8119519e921f67e00832f5b5e3175e2dd095ee13
Ashwani <ashwani90125@gmail.com>
2025-02-05 18:06:30 +0530

    Work on product slice setup

--- cha_15/product.go
+++ cha_15/product.go
@@ -9,20 +9,19 @@ type Product struct {
 
 type ProductSlice []*Product
 
-var Products []*Product
 
 var Products = ProductSlice {
-	{"Kayak", "Watersports", 279}
-	{ "lifeJacket", "Watersports", 300 }
-	{ "lifeJacket2", "Watersports", 360 }
-	{ "lifeJacket3", "Watersports", 350 }
-	{ "lifeJacke4", "Watersports", 340 }
-	{ "lifeJacket4", "Watersports", 330 }
-	{ "lifeJacket5", "Watersports", 320 }
-	{ "lifeJacket6", "Watersports", 310 }
+	{"Kayak", "Watersports", 279},
+	{ "lifeJacket", "Watersports", 300 },
+	{ "lifeJacket2", "Watersports", 360 },
+	{ "lifeJacket3", "Watersports", 350 },
+	{ "lifeJacke4", "Watersports", 340 },
+	{ "lifeJacket4", "Watersports", 330 },
+	{ "lifeJacket5", "Watersports", 320 },
+	{ "lifeJacket6", "Watersports", 310 },
 }
 
-func ToCurrency(var float64) string {
+func ToCurrency(val float64) string {
 	return "$" + strconv.FormatFloat(val, 'f', 2, 64)
 }
 
