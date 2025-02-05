6dfc7b8740dfb456643b9338619887ece0ac8698
Ashwani <ashwani90125@gmail.com>
2025-02-05 18:00:25 +0530

    Work on product slice setup

---
+++ cha_15/product.go
@@ -0,0 +1,28 @@
+package main
+
+import "strconv"
+
+type Product struct {
+	Name, Category string
+	Price float64
+}
+
+type ProductSlice []*Product
+
+var Products []*Product
+
+var Products = ProductSlice {
+	{"Kayak", "Watersports", 279}
+	{ "lifeJacket", "Watersports", 300 }
+	{ "lifeJacket2", "Watersports", 360 }
+	{ "lifeJacket3", "Watersports", 350 }
+	{ "lifeJacke4", "Watersports", 340 }
+	{ "lifeJacket4", "Watersports", 330 }
+	{ "lifeJacket5", "Watersports", 320 }
+	{ "lifeJacket6", "Watersports", 310 }
+}
+
+func ToCurrency(var float64) string {
+	return "$" + strconv.FormatFloat(val, 'f', 2, 64)
+}
+
