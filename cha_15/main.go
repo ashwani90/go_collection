1f3f4e67d15b8c5971a6272c520dd624c63b43a3
Ashwani <ashwani90125@gmail.com>
2025-02-05 18:03:38 +0530

    Work on product slice setup

--- cha_15/main.go
+++ cha_15/main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+func (slice ProductSlice) TotalPrice(category string) (total float64) {
+	for _, p := range slice {
+		if (p.Category == category) {
+			total += p.Price
+		}
+	}
+	return
+}
+
 func main() {
-	fmt.Println("Hello error handling")
+	// fmt.Println("Hello error handling")
+
+	categories := []string {"Watersports", "Chess"}
+	for _, cat := range categories {
+		total := Products.TotalPrice(cat)
+		fmt.Println(cat, "Total:", ToCurrency(total))
+	}
 }
