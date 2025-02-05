10247124a0ad9530132cdc998c0fe84de80ace91
Ashwani <ashwani90125@gmail.com>
2025-02-05 18:07:14 +0530

    Work on product slice setup

--- cha_15/main.go
+++ cha_15/main.go
@@ -2,15 +2,6 @@ package main
 
 import "fmt"
 
-func (slice ProductSlice) TotalPrice(category string) (total float64) {
-	for _, p := range slice {
-		if (p.Category == category) {
-			total += p.Price
-		}
-	}
-	return
-}
-
 func main() {
 	// fmt.Println("Hello error handling")
 
