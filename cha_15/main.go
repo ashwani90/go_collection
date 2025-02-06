58280615d9c19ccf3dc4d983ebaca6c0669ea09d
Ashwani <ashwani90125@gmail.com>
2025-02-06 17:29:02 +0530

    Work on error handling

--- cha_15/main.go
+++ cha_15/main.go
@@ -1,8 +1,44 @@
 package main
 
+
 import "fmt"
 
+type CategoryCountMessage struct {
+	Category string
+	Count int
+}
+
+func processCategories(categories []string, outChan chan <- CategoryCountMessage) {
+	defer func() {
+		if arg := recover(); arg != nil {
+			fmt.Println(arg)
+		}
+	}()
+	channel := make(chan ChannelMessage, 10)
+	go Products.TotalPriceAsync(categories, channel)
+	for message := range channel {
+		if message.CategoryError == nil {
+			outChan <- CategoryCountMessage {
+				Category: message.Category,
+				Count: int(message.Total),
+			}
+		} else {
+			panic(message.CategoryError)
+		}
+	}
+	close(outChan)
+}
+
 func main() {
+	categories := []string {"Watersports", "Chess", "Running" }
+	channel := make(chan CategoryCountMessage)
+	go processCategories(categories, channel)
+	for message := range channel {
+		fmt.Println(message.Category, "Total:", message.Count)
+	}
+}
+
+func main3() {
 
 	
 	// recoveryFunc := func() {
