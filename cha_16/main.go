d294408db611fafc5a6af3accabf6e7137449fd8
Ashwani <ashwani90125@gmail.com>
2025-02-07 05:14:00 +0530

    Work on add setup for string

--- cha_16/main.go
+++ cha_16/main.go
@@ -3,9 +3,15 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
 	product := "Kayak"
-	fmt.Println("Product:", product)
+	fmt.Println("Contains:", strings.Contains(product, "yak"))
+	fmt.Println("Contains Any:", strings.ContainsAny(product, "abc"))
+	fmt.Println("ContainsRune:", strings.ContainsRune(product, "K"))
+	fmt.Println("EqualFold:", strings.EqualFold(product, "KAYAK"))
+	fmt.Println("HasPrefix:", strings.HasPrefix(product, "Ka"))
+	fmt.Println("HasSuffix:", strings.HasSuffix(product, "yak"))
 }
