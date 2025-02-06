c11c77a1a9e7247970148b3b331f2a52c8a9337f
Ashwani <ashwani90125@gmail.com>
2025-02-07 05:18:01 +0530

    Work on string operations

--- cha_16/main.go
+++ cha_16/main.go
@@ -23,4 +23,6 @@ func main() {
 	fmt.Println("Original:", specialChar, []byte(specialChar))
 	upperChar := strings.ToUpper(specialChar)
 	fmt.Println("Upper:", upperChar, []byte(upperChar))
+	titleChar := strings.ToTitle(specialChar)
+	fmt.Println("Title:", titleChar, []byte(titleChar))
 }
