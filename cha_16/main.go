667717c961d6e73ace86cbc05c6da69dc0c80296
Ashwani <ashwani90125@gmail.com>
2025-02-07 05:14:27 +0530

    Work on string operations

--- cha_16/main.go
+++ cha_16/main.go
@@ -10,7 +10,7 @@ func main() {
 	product := "Kayak"
 	fmt.Println("Contains:", strings.Contains(product, "yak"))
 	fmt.Println("Contains Any:", strings.ContainsAny(product, "abc"))
-	fmt.Println("ContainsRune:", strings.ContainsRune(product, "K"))
+	fmt.Println("ContainsRune:", strings.ContainsRune(product, 'K'))
 	fmt.Println("EqualFold:", strings.EqualFold(product, "KAYAK"))
 	fmt.Println("HasPrefix:", strings.HasPrefix(product, "Ka"))
 	fmt.Println("HasSuffix:", strings.HasSuffix(product, "yak"))
