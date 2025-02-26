93a20ff2a7583f887bb7ad2abc5e4593ceb8e585
Ashwani <ashwani90125@gmail.com>
2025-02-26 19:54:05 +0530

    Work on replaces text function

--- cha_16/main.go
+++ cha_16/main.go
@@ -131,4 +131,17 @@ func main() {
 	} else {
 		fmt.Println("Error: ", err)
 	}
+
+	pattern, compileErr := regexp.Compile("[A-z]oat")
+	description = "A boat for one person"
+	question := "Is that a goat?"
+	preference := "I like oats"
+
+	if (compileErr == nil) {
+		fmt.Println("Description: ", pattern.MatchString(description))
+		fmt.Println("Question:", pattern.MatchString(question))
+		fmt.Println("Preference:", pattern.MatchString(preference))
+	} else {
+		fmt.Println("Error: ", compileErr)
+	}
 }
