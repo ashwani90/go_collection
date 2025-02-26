0dcc8abeb8c3f6c235369816a5f1805dcf861ef9
Ashwani <ashwani90125@gmail.com>
2025-02-26 19:59:57 +0530

    Work on replaces text function

--- cha_16/main.go
+++ cha_16/main.go
@@ -8,6 +8,10 @@ import (
 	"unicode"
 )
 
+func getSubstring(s string, indices []int) string {
+	return string(s[indices[0]:indices[1]])
+}
+
 func main() {
 	product := "Kayak"
 	fmt.Println("Contains:", strings.Contains(product, "yak"))
@@ -143,5 +147,15 @@ func main() {
 		fmt.Println("Preference:", pattern.MatchString(preference))
 	} else {
 		fmt.Println("Error: ", compileErr)
+	}
+
+	pattern = regexp.MustCompile("K[a-z]{4}|[A-z]oat")
+	description = "Kayak. A boat for one person"
+	firstIndex := pattern.FindStringIndex(description)
+	allIndices := pattern.FindAllStringIndex(description, -1)
+	fmt.Println("First index", firstIndex[0], "-", firstIndex[1], "=", getSubstring(description, firstIndex))
+	
+	for i, idx := range allIndices {
+		fmt.Println("Index", i, "=", idx[0], "-", idx[1], "=", getSubstring(description, idx))
 	}
 }
