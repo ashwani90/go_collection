31cb3b0d09b30fb71469fc9c8287af6b48f5c0f8
Ashwani <ashwani90125@gmail.com>
2025-02-27 16:24:51 +0530

    Work on fix add regexp

--- cha_17/main.go
+++ cha_17/main.go
@@ -3,6 +3,30 @@ package main
 
 import "fmt"
 
+type Product struct {
+	Name, Category string
+	Price float64
+}
+
+var Kayak = Product{
+	Name: "Kayak",
+	Category: "Watersports",
+	Price: 275,
+}
+
+var Products =[]Product {
+	{"Keyak", "watersports", 279},
+	{"Lifejacket", "watersports", 279},
+	{"Soccer Ball", "Soccer", 279},
+	{"Corner Flags", "Soccer", 279},
+	{"Stadium", "Soccer", 279},
+	{"Thinking Cap", "Chess", 279},
+	{"Unsteady Chair", "Chess", 279},
+	{"Bling", "Chess", 279},
+	{"King", "Chess", 279},
+}
+
+
 func main() {
 	fmt.Println("Hello formatting strings")
 	fmt.Println("Product:", Kayak.Name, "Price:", Kayak.Price)
