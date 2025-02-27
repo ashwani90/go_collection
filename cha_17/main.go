71cd2d98ac5a55cea9ec45e3b01a02d5d51fc009
Ashwani <ashwani90125@gmail.com>
2025-02-27 16:30:04 +0530

    Work on fix add regexp

--- cha_17/main.go
+++ cha_17/main.go
@@ -27,9 +27,24 @@ var Products =[]Product {
 }
 
 
+func getProductName(index int) (name string,  err error) {
+	if (len(Products) > index) {
+		name = fmt.Sprintf("Name of product: %v", Products[index].Name)
+	} else {
+		err = fmt.Errorf("Error for index %v", index)
+	}
+	return
+}
+
 func main() {
 	fmt.Println("Hello formatting strings")
 	fmt.Println("Product:", Kayak.Name, "Price:", Kayak.Price)
 
 	fmt.Printf("Product: %v, Price: $%4.2f", Kayak.Name, Kayak.Price)
+
+	name, _ := getProductName(1)
+	fmt.Println(name)
+
+	_, err := getProductName(10)
+	fmt.Println(err.Error())
 }
