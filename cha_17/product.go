3644d271ba172b2490efc847a66972ef8f9f78d3
Ashwani <ashwani90125@gmail.com>
2025-02-27 16:18:40 +0530

    Work on fix add regexp

---
+++ cha_17/product.go
@@ -0,0 +1,24 @@
+package main
+
+type Product struct {
+	Name, Category string
+	Price float64
+}
+
+var Kayak = Product {
+	Name: "Kayak",
+	Category: "Watersports",
+	Price: 275,
+}
+
+var Products =[]Product {
+	{"Keyak", "watersports", 279},
+	{"Lifejacket", "watersports", 279},
+	{"Soccer Ball", "Soccer", 279},
+	{"Corner Flags", "Soccer", 279},
+	{"Stadium", "Soccer", 279},
+	{"Thinking Cap", "Chess", 279},
+	{"Unsteady Chair", "Chess", 279},
+	{"Bling", "Chess", 279},
+	{"King", "Chess", 279},
+}
